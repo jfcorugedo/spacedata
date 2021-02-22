83bc9f6e8f89f94510679ae6a842c4b64104c541
jfcorugedo <jfcorugedo@gmail.com>
2021-02-22 17:22:53 +0100

    chore: first version of the project with webflux and base dependencies

---
+++ src/main/java/com/jfcorugedo/spacedata/SpacedataApplication.java
@@ -0,0 +1,13 @@
+package com.jfcorugedo.spacedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacedataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfcorugedo/spacedata/SpacedataApplicationTests.java
@@ -0,0 +1,15 @@
+package com.jfcorugedo.spacedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RocketsResourceTest {
+
+	@Test
+	void getOneRocket() {
+	}
+
+}
